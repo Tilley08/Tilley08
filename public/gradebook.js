8d86409b0e4aee95066b31296df40214d4ab4a10
Tilley08 <nstilley@my.waketech.edu>
2025-04-11 16:20:03 -0400

    Create gradebook.js
    public folder

---
+++ public/gradebook.js
@@ -0,0 +1,17 @@
+// TODO: Fetch data from the postgreSQL database (to be implemented later)
+function fetchGradebookData() {
+    // This funnction will query the PostgreSQL database and return grade data
+    console.log("Fetching grade data...");
+}
+
+// TODO: Populate the table with grade data
+fuunction populateGradebook(data) {
+    // This funtion will take the fetched data and populate the gradebook table
+    console.log("Populating gradebook with data...", data);
+}
+
+// TODO:REMOVE THIS
+//: Call the stubs to demonsatrate the workflow
+const GradeData = fetchGradeData();
+populateGradebook(GradeData);
+// END REMOVE
